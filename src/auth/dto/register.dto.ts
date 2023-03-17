fb86392f5bf25c38891d784328a3897e4e570279
abimanyusatria665@gmail.com <abimanyusatria665@gmail.com>
2023-03-17 10:21:12 +0700

    first push

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Gender,  Prisma } from '@prisma/client';
+import { IsEmpty, IsNotEmpty } from 'class-validator';
+
+export class RegisterDto implements Prisma.usersCreateInput {
+  @ApiProperty()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  gender: Gender;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  phone_num: string;
+
+  @ApiProperty()
+  @IsEmpty()
+  media_id: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  password: string;
+}
